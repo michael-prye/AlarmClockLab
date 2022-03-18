614dc2ff28b5a937c8820c7c8e3e47ec57e39211
michael-prye <michael.prye@gmail.com>
2022-03-18 14:13:48 -0400

    Final commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from alarmClock import Alarm_clock
 
-my_alarm_clock = Alarm_clock('12:00')
-my_alarm_clock.get_time()
-my_alarm_clock.set_time()
-my_alarm_clock.set_alarm()
-my_alarm_clock.get_alarm_status()
+
+def run():
+    my_alarm_clock = Alarm_clock('12:00')
+    my_alarm_clock.get_time()
+    my_alarm_clock.set_time()
+    my_alarm_clock.set_alarm()
+    my_alarm_clock.get_alarm_status()
+run()
